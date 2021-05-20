4d375e1fb62a20b1c43c0facf9cbda6a4290d25c
Nimisha-12345 <nimishas082@gmail.com>
2021-05-20 13:54:37 +0530

    code formatted

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,73 +1,68 @@
 <template>
-    <div class="header">
-        <!-- <h2 style="white-space:pre-line">{{message}}</h2> -->
-         <div :class="{red: isRed}">
-                {{message}} 
-        </div>
-        <div class="section">
-        <div class="texts">
-        <textarea type="text" v-model="message" />
-        </div>
-        
-        <button v-on:click="clear">Clear</button>
-        <button v-on:click="makeCaptial">Cap</button>
-         <input :checked="isRed" v-model="isRed" type="checkbox" @click="red" />
-        </div>
-        
-    
+  <div class="header">
+    <!-- <h2 style="white-space:pre-line">{{message}}</h2> -->
+    <div :class="{ red: isRed }">
+      {{ message }}
     </div>
+    <div class="section">
+      <div class="texts">
+        <textarea type="text" v-model="message" />
+      </div>
+
+      <button v-on:click="clear">Clear</button>
+      <button v-on:click="makeCaptial">Cap</button>
+      <input :checked="isRed" v-model="isRed" type="checkbox" @click="red" />
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name:'main',
-    data(){
-        return{
-            message:'Hello Vue I am ))))',
-             isRed: false,
-
-        };
+  name: "main",
+  data() {
+    return {
+      message: "Hello Vue I am ))))",
+      isRed: false,
+    };
+  },
+  created: function () {
+    console.log("created");
+  },
+  mounted: function () {
+    console.log("mounted");
+  },
+  methods: {
+    clear: function () {
+      this.message = "";
     },
-    created: function(){
-       console.log("created");
+    makeCaptial: function () {
+      this.message = this.message.toUpperCase();
     },
-    mounted: function(){
-        console.log("mounted");
+    red: function () {
+      console.log(this.isRed);
     },
-    methods:{
-        clear: function(){
-            this.message= "";
-        },
-        makeCaptial: function(){
-            this.message = this.message.toUpperCase();
-        },
-         red: function() {
-        console.log(this.isRed)}
-    }
-}
+  },
+};
 </script>
 
 <style>
-.section{
-    display: flex;
-    justify-content: center;
-    align-items: center;
+.section {
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
-button{
-    margin-top: -3px;
-    padding-left: 10px;
-    padding-top: 12px;
-    padding-right: 10px;
-    padding-bottom: 6px;
+button {
+  margin-top: -3px;
+  padding-left: 10px;
+  padding-top: 12px;
+  padding-right: 10px;
+  padding-bottom: 6px;
 }
 .red {
   color: red;
 }
-.green {
-  color: green;
-}
-input[type=checkbox]{
-    height: 40px;
-    width: 40px;
+input[type="checkbox"] {
+  height: 40px;
+  width: 40px;
 }
 </style>
