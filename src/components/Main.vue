76a32bb7a74c5fb17b191ff44cef8e3eae9f7911
Nimisha-12345 <nimishas082@gmail.com>
2021-05-20 10:22:58 +0530

    styling

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,11 +1,14 @@
 <template>
     <div class="header">
         <h2 style="white-space:pre-line">{{message}}</h2>
-        
+        <div class="section">
+        <div class="texts">
         <textarea type="text" v-model="message" />
+        </div>
         
         <button v-on:click="clear">Clear</button>
         <button v-on:click="makeCaptial">Cap</button>
+        </div>
     
     </div>
 </template>
@@ -29,3 +32,18 @@ export default {
     }
 };
 </script>
+
+<style>
+.section{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+button{
+    margin-top: -3px;
+    padding-left: 10px;
+    padding-top: 12px;
+    padding-right: 10px;
+    padding-bottom: 6px;
+}
+</style>
