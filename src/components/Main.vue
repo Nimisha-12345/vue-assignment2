26203cb945ed878dac0bb6b44ea724b207622919
Nimisha-12345 <nimishas082@gmail.com>
2021-05-20 10:06:22 +0530

    initial commit

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="header">
         <h2 style="white-space:pre-line">{{message}}</h2>
+        
         <textarea type="text" v-model="message" />
+        
+        <button v-on:click="clear">Clear</button>
+        <button v-on:click="makeCaptial">Cap</button>
+    
     </div>
 </template>
 
@@ -13,6 +18,14 @@ export default {
             message:'Hello Vue I am ))))',
 
         }
+    },
+    methods:{
+        clear: function(){
+            this.message= "";
+        },
+        makeCaptial: function(){
+            this.message = this.message.toUpperCase();
+        }
     }
-}
+};
 </script>
